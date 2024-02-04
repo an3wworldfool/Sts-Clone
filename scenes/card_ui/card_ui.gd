230ee086510d58c9f07bc76a13cc48d555e71394
an3wworldfool <ing.germanspdf@gmail.com>
2024-02-04 11:34:17 -0700

    fixed the clicking bug

--- scenes/card_ui/card_ui.gd
+++ scenes/card_ui/card_ui.gd
@@ -7,6 +7,7 @@ signal reparent_requested(which_card_ui:CardUI)
 @onready var state: Label = $State
 @onready var drop_point_detector: Area2D = $DropPointDetector
 @onready var card_state_machine: CardStateMachine = $CardStateMachine as CardStateMachine  
+@onready var targets: Array[Node] = []
 
 func _ready() -> void:
 	card_state_machine.init(self)
@@ -21,9 +22,12 @@ func _on_mouse_entered() -> void:
 	card_state_machine.on_mouse_entered()
 	
 func _on_mouse_exited() -> void:
-	card_state_machine.on_mouse_exited()
-	
+	card_state_machine.on_mouse_exited()	
 
-	
+func _on_drop_point_detector_area_entered(area):
+	if not targets.has(area):
+		targets.append(area)
 
-	
+
+func _on_drop_point_detector_area_exited(area):
+	targets.erase(area)
