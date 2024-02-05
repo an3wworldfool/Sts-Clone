1e96cdad54dd43c0cbe0b166b74af3935456dd1f
an3wworldfool <ing.germanspdf@gmail.com>
2024-02-05 10:39:18 -0700

    Added a targeting system

--- scenes/ui/hand.gd
+++ scenes/ui/hand.gd
@@ -6,7 +6,9 @@ extends HBoxContainer
 func _ready():
 	for child in get_children():
 		var card_ui := child as CardUI
+		card_ui.parent = self
 		card_ui.reparent_requested.connect(_on_card_ui_reparent_requested)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_card_ui_reparent_requested(child: CardUI) -> void:
 	child.reparent(self)
